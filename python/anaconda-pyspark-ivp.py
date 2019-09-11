8f9f24054b699090773323d9c42e0ee366b7a003
Connor Hayes <30296185+connor-t-hayes@users.noreply.github.com>
2019-09-11 14:13:55 -0400

    Added Updated IVP Code

--- python/anaconda-pyspark-ivp.py
+++ python/anaconda-pyspark-ivp.py
@@ -1,17 +1,14 @@
-# IBM z Open Data Analytics (IzODA)
 # Installation Verification Program (IVP)
 # Copyright IBM 2017
 
 # izodaIVP.py
-# Last updated 08/05/2017
+# Last updated 10/12/2017
 #
 # Simple analysis on a VSAM file
 # VSAM file contains Client Retention Demo
 
 import sys
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField
-from pyspark.sql.types import DoubleType, IntegerType, StringType
 import pandas
 
 helpText = \
@@ -23,13 +20,11 @@ helpText = \
 if __name__ == "__main__":
 
     # Check and process parameters
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 2:
         print(helpText)
         sys.exit(-1)
     else:
-        mdssURL = sys.argv[0]
-        user = sys.argv[1]
-        password = sys.argv[2]
+        mdssURL = sys.argv[1]
 
     # Initialize Spark
     spark = SparkSession \
@@ -37,39 +32,30 @@ if __name__ == "__main__":
         .appName("izodaIVP") \
         .config("spark.files.overwrite", "true") \
         .getOrCreate()
-
-    schema = StructType([
-        StructField("CONT_ID", IntegerType()),
-        StructField("GENDER", IntegerType()),
-        StructField("AGE_YEARS", DoubleType()),
-        StructField("HIGHEST_EDU", IntegerType()),
-        StructField("ANNUAL_INVEST", DoubleType()),
-        StructField("ANNUAL_INCOME", IntegerType()),
-        StructField("ACTIVITY_LEVEL", IntegerType()),
-        StructField("CHURN", IntegerType()),
-    ])
+        
 
     # Read MDSS Client Retention VSAM file and place that data into a dataframe
-    clientIncomeDF = spark.read \
+    StaffDF = spark.read \
         .format("jdbc") \
+        .option("driver", "com.rs.jdbc.dv.DvDriver") \
         .option("url", mdssURL) \
-        .option("dbtable", "CLIENT_RETN") \
-        .option("user", user) \
-        .option("password", password) \
+        .option("dbtable", "STAFFVS") \
+        .option("user", "") \
+        .option("password", "") \
         .load()
 
     # Group dataframe values by education level
     # Aggregate values on multiple columns
-    avgByEducationDF = clientIncomeDF \
-        .groupBy("HIGHEST_EDU") \
-        .avg("AGE_YEARS", "ANNUAL_INVEST", "ANNUAL_INCOME") \
-        .orderBy("HIGHEST_EDU")
+    avgByDeptDF = StaffDF \
+	.groupBy("STAFFVS_DATA_DEPT") \
+        .avg("STAFFVS_DATA_YRS","STAFFVS_DATA_NAME_L") \
+	.orderBy("STAFFVS_DATA_DEPT")
 
     # Print all results
-    avgByEducationDF.show(avgByEducationDF.count())
+    avgByDeptDF.show()
     
     # Convert Spark dataframe to pandas dataframe and transpose
-    print(avgByEducationDF.toPandas().transpose())
+    print(avgByDeptDF.toPandas().transpose())
 
     # Stop Spark Session
     spark.stop()
